fad0f4d847e8c0453b02d58996e608e4366c962a
sparku7 <sparku7@sky.com>
2024-06-25 13:58:28 +0100

    Connected to H2 DB

--- src/main/java/com/qa/LBG_Srping_2/entities/Person.java
+++ src/main/java/com/qa/LBG_Srping_2/entities/Person.java
@@ -1,45 +1,60 @@
 package com.qa.LBG_Srping_2.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Person {
 
+    @Id
+    @GeneratedValue
+    private Integer id;
     private String name;
     private int age;
     private String job;
 
-    // Constructor to initialize the private properties
-    public Person(String name, int age, String job) {
+    public Person(Integer id, String name, int age, String job) {
+        this.id = id;
         this.name = name;
         this.age = age;
         this.job = job;
+
     }
 
-    // Getter method for the 'name' property
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
 
-    // Setter method for the 'name' property
     public void setName(String name) {
         this.name = name;
     }
 
-    // Getter method for the 'age' property
     public int getAge() {
         return age;
     }
 
-    // Setter method for the 'age' property
     public void setAge(int age) {
         this.age = age;
     }
 
-    // Getter method for the 'job' property
     public String getJob() {
         return job;
     }
 
-    // Setter method for the 'job' property
     public void setJob(String job) {
         this.job = job;
     }
 }
+
+
+
